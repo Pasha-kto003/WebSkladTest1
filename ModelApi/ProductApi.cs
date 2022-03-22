48a88175958ff1aa39fc63b499f4863978ebe109
Паша Луковников <lukovnikov.pasha@gmail.com>
2022-03-22 16:04:03 +1000

    ++

--- ModelApi/ProductApi.cs
+++ ModelApi/ProductApi.cs
@@ -16,10 +16,13 @@ namespace ModelApi
         public int? UnitId { get; set; }
         public int? ProductTypeId { get; set; }
         public string Status { get; set; }
+
         public UnitApi Unit { get; set; }
         public ProductTypeApi ProductType { get; set; }
         public CrossProductOrderApi CrossProductOrderApi { get; set; }
         public CrossOrderOutApi CrossOrderOutApi { get; set; }
+        public CrossProductRackApi CrossProductRack { get; set; }
+
         public int? CountProducts { get; set; }
         public int? CountProductsOut { get; set; }
     }
